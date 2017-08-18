fb6d4da1abd5ac31d22b8937acb1220676d1cba9
arfin97 <arfin97@gmail.com>
2017-08-19 02:42:35 +0600

    Create 11332 Summing Digits.cpp

---
+++ 11332 Summing Digits.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define debug(x) cout << #x << " = " << (x) << endl
+#define fr freopen("in.txt", "r", stdin);
+#define fw freopen("out.txt", "w", stdout);
+#define mem(x) memset((x), 0, sizeof((x)));
+
+ int divisible_by_three(string num){
+    int temp = 0;
+    for(int i = 0; i < num.length(); i++) temp += (num[i]-'0');
+    debug(temp);
+    int number = temp;
+    while(temp > 9){
+        //debug(temp);
+        temp = 0;
+        while(number != 0){
+            temp += (number%10);
+            number/=10;
+            //debug(number);
+        }
+        number = temp;
+    }
+    return temp;
+}
+
+int main(){
+    string str;
+    while((cin >> str)){
+        if(str == "0") break;
+        int result = divisible_by_three(str);
+        printf("%d\n", result);
+    }
+    return 0;
+}
